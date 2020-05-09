9b8b83831ed0fb3309a7d743b9d995db670dc911
Jay Meck <jmeck@p-r-i.org>
2020-05-09 16:35:56 -0400

    Testing

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
+from flask_migrate import Migrate
 
 database_path = os.environ['DATABASE_URL']
 
@@ -12,11 +13,11 @@ setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
 def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config.from_object('config')
     db.app = app
     db.init_app(app)
     db.create_all()
+    migrate = Migrate(app, db)
 
 
 '''
