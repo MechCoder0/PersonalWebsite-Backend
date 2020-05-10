a51c617de02664bee1a76b98db1f3bc9aa8981d3
Jay Meck <jmeck@p-r-i.org>
2020-05-10 15:37:30 -0400

    Updated manage.py

--- manage.py
+++ manage.py
@@ -3,11 +3,9 @@ from flask_migrate import Migrate, MigrateCommand
 from app import app
 from models import db
 
+print(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
-
 manager.add_command('db', MigrateCommand)
-
-
 if __name__ == '__main__':
     manager.run()
